f5bc3f002d308d13a65ee75a1976a6e314c254e2
Cristiam Sosa <cristiam.sosa@innocv.com>
2018-11-27 20:38:58 +0100

    initial commit

---
+++ src/main/java/com/innocv/technicaltest2/TechnicalTest2Application.java
@@ -0,0 +1,12 @@
+package com.innocv.technicaltest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalTest2Application.class, args);
+	}
+}
